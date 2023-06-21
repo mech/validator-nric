44871d51726f1aed9c731db71ace3701c6dd5442
JeremyC-za <jcoupland12@gmail.com>
2023-06-21 17:17:49 +0200

    Changing how errors are added to records

--- lib/validator/nric/nric_validator.rb
+++ lib/validator/nric/nric_validator.rb
@@ -2,7 +2,7 @@ module Validator
   module Nric
     class NricValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
-        record.errors[attribute] << (options[:message] || 'is not an NRIC') \
+        record.errors.add(attribute, options[:message] || 'is not an NRIC') \
           unless Validator::Nric.check(value)
       end
     end
